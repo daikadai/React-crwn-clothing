03843b333c1087baa10b7cd5768adba57aef66b4
Pham Cao Dai <daikadai1026@gmail.com>
2020-06-02 00:19:37 +0700

    56. Adding Shop Data to Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,16 +3,19 @@ import CollectionOverview from '../../components/collection-overview/collection-
 import { Route } from 'react-router-dom'
 import  CollectionPage  from '../collection/collection.component' 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+      const { updateCollections } = this.props
       const collectionRef = firestore.collection('collections');
       collectionRef.onSnapshot(async snapshot => {
-        convertCollectionsSnapshotToMap(snapshot);
-        
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
       })
     }
     render() {
@@ -26,6 +29,8 @@ class ShopPage extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
 
-
-export default ShopPage;
+export default connect(null,mapDispatchToProps)(ShopPage);
