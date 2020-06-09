974e3ebf15043645336e6b2d785f31a13cc64a11
Pham Cao Dai <daikadai1026@gmail.com>
2020-06-09 22:06:48 +0700

    59. Promise Pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -19,9 +19,10 @@ class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-      const { updateCollections } = this.props
+      const { updateCollections } = this.props;
       const collectionRef = firestore.collection('collections');
-      collectionRef.onSnapshot(async snapshot => {
+
+      collectionRef.get().then(snapshot => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionsMap);
         this.setState({ loading: false });
