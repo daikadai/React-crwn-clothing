e95d6a406c0be8889f067a14ac18f96490d0110c
Pham Cao Dai <daikadai1026@gmail.com>
2020-06-20 11:11:27 +0700

    63. redux-sage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import persistStore from "redux-persist/es/persistStore";
-import thunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
+import { fetchCollectionsStart } from "./shop/shop.sagas";
 
-const middlewares = [thunk, logger];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 export const persistor = persistStore(store);
 export default {store, persistor};
  
