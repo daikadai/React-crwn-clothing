a448e14c5977eaabfc1a03df612322955fcc94b3
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-21 14:14:33 +0700

    65. Root Saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import persistStore from "redux-persist/es/persistStore";
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -11,7 +11,7 @@ const middlewares = [sagaMiddleware, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
 export default {store, persistor};
  
